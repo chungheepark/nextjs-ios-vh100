0d3eef5f65c94cf5b81985bb2b0c943c54ac5f82
chungheepark <dev.roy.park@gmail.com>
2020-09-16 21:44:57 +0900

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,34 @@
 import '../styles/globals.css'
+import {useCallback, useEffect} from "react";
+import debounce from 'lodash/debounce';
 
-function MyApp({ Component, pageProps }) {
+const setVw2Root = () =>
+  document.querySelector(':root')
+    .style
+    .setProperty('--vw', window.innerWidth / 100 + 'px')
+
+const setVh2Root = () =>
+  document.querySelector(':root')
+    .style
+    .setProperty('--vh', window.innerHeight / 100 + 'px')
+
+function MyApp({Component, pageProps}) {
+
+  const debouncedResizeCallback = useCallback(debounce(() => {
+    console.log('resize')
+    setVw2Root()
+    setVh2Root()
+  }, 50), [])
+
+  useEffect(() => {
+    setVw2Root()
+    setVh2Root()
+
+    window.addEventListener('resize', debouncedResizeCallback)
+
+    return () => window.removeEventListener('resize', debouncedResizeCallback)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
